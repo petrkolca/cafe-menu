bb0a4bfc0cd944454062d0de055d809edf111a3b
Petr Kolarik <kolarik.petr@outlook.com>
2023-02-27 19:41:44 +1100

    Passing all Unigue categories + listing as buttons

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 import { StyledFilterCnt } from './styles/Filter.styled.jsx'
 
-const Categories = ({filterItems}) => {
-
+const Categories = ({filterItems, allCategories}) => {
 
     return (
         <StyledFilterCnt>
-            <button 
-                className="filter-btn"
-                onClick={ () => filterItems('all')}
-            >
-                All
-            </button>
-            <button 
-                className="filter-btn"
-                onClick={ () => filterItems('breakfast')}
-            >
-                Breakfast
-            </button>
+            {allCategories.map((menuCategory, index) => {
+                return (
+                    <button 
+                        key={index}
+                        className="filter-btn"
+                        onClick={ () => filterItems(menuCategory)}
+                    >
+                        {menuCategory}
+                    </button>
+                )    
+            })}
         </StyledFilterCnt>
     );
 }
